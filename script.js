5f34bbb2cf8f16dbd9492e560e831b2d7aa00f12
jjdlabee <157652340+jjdlabee@users.noreply.github.com>
2024-04-11 17:12:12 +0000

    11/04/24 noon

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function checklogin(){
 
   const header = document.querySelector('#header');
 
-  html = `
-    <button class="btnLogin-popup" id="login">Login</button>
-  `;
+  // html = `
+  //    <button class="btnLogin-popup" id="login">Login</button>
+  // `;
 
   if(i === 0){
     header.innerHTML = header.innerHTML + html;
@@ -18,24 +18,16 @@ checklogin();
 
 
 
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close')
+// const wrapper = document.querySelector('.wrapper');
+// const loginLink = document.querySelector('.login-link');
+// const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
+// registerLink.addEventListener('click', ()=> {
+//     wrapper.classList.add('active');
+// });
 
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
+// loginLink.addEventListener('click', ()=> {
+//     wrapper.classList.remove('active');
+// });
 
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-});
