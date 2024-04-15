add029ff308fc3d7b7482d0bafb8b2164bfd227e
Jonathan La Borde <jjdlabee@gmail.com>
2024-04-15 16:52:42 +0000

    Added firebase files

---
+++ public/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4T_Y6Y-EC1pNmJ5hzlc5ctfxL4U6yjK4",
+    authDomain: "info-1601-project-a915b.firebaseapp.com",
+    projectId: "info-1601-project-a915b",
+    storageBucket: "info-1601-project-a915b.appspot.com",
+    messagingSenderId: "617733193874",
+    appId: "1:617733193874:web:9b92fbb596a6989034264b"
+  };
+
+  export default firebaseConfig;
